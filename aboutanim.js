2af3d56fb527c0bfe78dd5820f0d293a79f0c8b6
smoyamedina <smoyamedina@outlook.com>
2020-10-02 11:22:02 -0400

    new development -- fresh

--- aboutanim.js
+++
@@ -1,7 +0,0 @@
-gsap.to(".callmesam", {
-    duration: 1.2,
-    text: "You can call me Sam!",
-    ease: "linear",
-    padSpace: true,
-}
-)
--- script.js
+++ script.js
@@ -1,207 +1,47 @@
-gsap.registerPlugin(ScrollTrigger);
-// ScrollTrigger.saveStyles(".title-proj1, .title, .title-proj2, .title-proj3, .title-proj4, .title-proj5, .title-proj6, .slides")
-
-ScrollTrigger.matchMedia({
-
-    "(max-width: 800px)": () => {
-        const leftJut = (projNum) => {
-            gsap.to(`.title-proj${projNum}`, {
-                ease: "power4.inOut",
-                clipPath: "inset(0% 0% 0% 0%)",
-                // x: "-9rem",
-                // right: "2rem",
-                backgroundColor: "rgba( 0, 0, 0, 0)",
-                scrollTrigger: {
-                    trigger:`.slide-proj${projNum}`,
-                    // markers: true,
-                    start: "top 50%",
-                    end: "bottom 50%",
-                    toggleActions: "play reverse play reverse",
-                }
-            }
-            )
-        
-        }
-        
-        leftJut(1)
-        leftJut(2)
-        leftJut(3)
-        leftJut(4)
-        leftJut(5)
-        leftJut(6)
-    },
-
-    "(min-width: 801px)": () => {
-
-        const leftJut = (projNum) => {
-            gsap.to(`.title-proj${projNum}`, {
-                ease: "power4.inOut",
-                clipPath: "inset(0% 0% 0% 0%)",
-                x: "-9rem",
-                // right: "2rem",
-                backgroundColor: "rgba( 0, 0, 0, 0)",
-                scrollTrigger: {
-                    trigger:`.slide-proj${projNum}`,
-                    // markers: true,
-                    start: "top 30%",
-                    end: "10%",
-                    // end: "bottom 20%",
-                    toggleActions: "play reverse play reverse",
-                }
-            }
-            )
-        
-        }
-        leftJut(1)
-        leftJut(2)
-        leftJut(3)
-        leftJut(4)
-        // leftJut(5)
-        // leftJut(6)
-
-        
-
-
-        let leftJutFive = () => {
-            gsap.to(".title-proj5", {
-                ease: "power4.inOut",
-                clipPath: "inset(0% 0% 0% 0%)",
-                x: "-9rem",
-                // right: "2rem",
-                backgroundColor: "rgba( 0, 0, 0, 0)",
-                scrollTrigger: {
-                    trigger:".slide-proj5",
-                    // markers: true,
-                    start: "top 30%",
-                    end: "bottom 60%",
-                    toggleActions: "play reverse play reverse",
-                }
-            }
-            )
-        }
-        leftJutFive()
-
-        let leftJutSix = () => {
-            gsap.to(".title-proj6", {
-                ease: "power4.inOut",
-                clipPath: "inset(0% 0% 0% 0%)",
-                x: "-9rem",
-                // right: "2rem",
-                backgroundColor: "rgba( 0, 0, 0, 0)",
-                scrollTrigger: {
-                    trigger:".slide-proj6",
-                    // markers: true,
-                    start: "top 60%",
-                    end: "25%",
-                    toggleActions: "play reverse play reverse",
-                }
-            }
-            )
-        }
-
-        leftJutSix()
-            ///////////////////////////////////////////////
-        const bodyTimeline = gsap.timeline({
-            scrollTrigger:{
-            trigger: ".slides",
-            start: "0%",
-            end: "bottom 50%",
-            toggleActions: "play reverse play reverse",
+let textReveal = (projNum, projTitle) => {
+    gsap.to (`.proj-name${projNum}`, {
+        duration: .8,
+        text: projTitle,
+        ease: "linear",
+        // x: 50,
+        scrollTrigger: {
+            trigger:`.slide${projNum}`,
             // markers: true,
-            scrub: 1
-            }
-        }
-        ).to(".title", {y: "-20rem"})
-
-    },
-
-    "(min-width: 1200px)": () => {
-
-        const leftJut = (projNum) => {
-            gsap.to(`.title-proj${projNum}`, {
-                ease: "power4.inOut",
-                clipPath: "inset(0% 0% 0% 0%)",
-                x: "-9rem",
-                // right: "2rem",
-                backgroundColor: "rgba( 0, 0, 0, 0)",
-                scrollTrigger: {
-                    trigger:`.slide-proj${projNum}`,
-                    // markers: true,
-                    start: "top 30%",
-                    end: "10%",
-                    toggleActions: "play reverse play reverse",
-                }
-            }
-            )
-        
-        }
-        
-        leftJut(1)
-        leftJut(2)
-        leftJut(3)
-        leftJut(4)
-        // leftJut(5)
-        // leftJut(6)
-        
-        const bodyTimeline = gsap.timeline({
-            scrollTrigger:{
-            trigger: ".slides",
-            start: "0%",
-            end: "bottom 50%",
+            start: "top 75%",
+            end: "bottom: -5%",
+            // end: "50%",
             toggleActions: "play reverse play reverse",
-            // markers: true,
-            scrub: 1
-            }
+            
         }
-        ).to(".title", {y: "-20rem"})
-        
-        
-
+    
     }
-   
+    )
+}
+textReveal(1, "the dry run")
+textReveal(2, "amass magazine")
+textReveal(3, "voter's guide")
+textReveal(4, "mailable poster")
+textReveal(5, "chapbook")
+textReveal(6, "misc materials")
 
-})
-
-
-////////////////////////
-
-// const bodyTimeline = gsap.timeline({
-//     scrollTrigger:{
-//     trigger: ".slides",
-//     start: "0%",
-//     end: "bottom 50%",
-//     toggleActions: "play reverse play reverse",
-//     // markers: true,
-//     scrub: 1
-//     }
-// }
-// ).to(".title", {y: "-20rem"})
-
-
-// const leftJut = (projNum) => {
-//     gsap.to(`.title-proj${projNum}`, {
-//         ease: "power4.inOut",
-//         clipPath: "inset(0% 0% 0% 0%)",
-//         x: "-9rem",
-//         // right: "2rem",
-//         backgroundColor: "rgba( 0, 0, 0, 0)",
-//         scrollTrigger: {
-//             trigger:`.slide-proj${projNum}`,
-//             // markers: true,
-//             start: "top 50%",
-//             end: "bottom 50%",
-//             toggleActions: "play reverse play reverse",
-//         }
-//     }
-//     )
-
-// }
-
-// leftJut(1)
-// leftJut(2)
-// leftJut(3)
-// leftJut(4)
-// leftJut(5)
-// leftJut(6)
-
-
+let projdescReveal = (projNum) => { 
+    gsap.from (`.proj-desc${projNum}`, {
+    duration: .8,
+    ease: "power2.inOut",
+    clipPath: "inset(0% 100% 0% 0%)",
+    scrollTrigger: {
+        trigger: `.slide${projNum}`,
+        start: "top 75%",
+        end: "bottom: -5%",
+        // markers: true,
+        toggleActions: "play reverse play reverse",
+    }
+}
+)
+}
+projdescReveal(1)
+projdescReveal(2)
+projdescReveal(3)
+projdescReveal(4)
+projdescReveal(5)
+projdescReveal(6)
