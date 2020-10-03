1d3502db158e20b2769aba51b6397381377e17af
smoyamedina <smoyamedina@outlook.com>
2020-10-02 22:07:48 -0400

    added 3rd project page template

--- sub-script.js
+++ sub-script.js
@@ -1,3 +1,5 @@
+// project 1 ///////////////////////
+
 gsap.to (".sub-proj-name1", {
     duration: 1,
     delay: .8,
@@ -7,7 +9,47 @@ gsap.to (".sub-proj-name1", {
 }
 )
 
-gsap.to (".sub-desc0", {
+gsap.to (".sub-desc1a", {
+    duration: 1,
+    delay: 1.8,
+    opacity: 1,
+    ease: "power3.inOut"
+    // x: 50,
+}
+)
+
+// project 2 ///////////////////////
+
+gsap.to (".sub-proj-name2", {
+    duration: 1,
+    delay: .8,
+    x: "32vw",
+    ease: "power4.Out"
+    // x: 50,
+}
+)
+
+gsap.to (".sub-desc2a", {
+    duration: 1,
+    delay: 1.8,
+    opacity: 1,
+    ease: "power3.inOut"
+    // x: 50,
+}
+)
+
+// project 3 ///////////////////////
+
+gsap.to (".sub-proj-name3", {
+    duration: 1,
+    delay: .8,
+    x: "-1.5vw",
+    ease: "power4.Out"
+    // x: 50,
+}
+)
+
+gsap.to (".sub-desc3a", {
     duration: 1,
     delay: 1.8,
     opacity: 1,
