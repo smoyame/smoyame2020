13eb48c0739050961a3ca785ac9fc27868540f5e
smoyamedina <smoyamedina@outlook.com>
2020-10-04 14:34:24 -0400

    swup test mobile push

--- transition.js
+++ transition.js
@@ -1,23 +1,23 @@
 
 
-const animationEnter = (container) => {
-    return gsap.from(container, {autoAlpha: 0, duration: 2, clearProps:'all', ease: 'none'})
-}
+// const animationEnter = (container) => {
+//     return gsap.from(container, {autoAlpha: 0, duration: 2, clearProps:'all', ease: 'none'})
+// }
 
-const animationLeave = (container) => {
-    return gsap.to(container, {autoAlpha: 0, duration: 2, clearProps:'all', ease: 'none'})
-}
+// const animationLeave = (container) => {
+//     return gsap.to(container, {autoAlpha: 0, duration: 2, clearProps:'all', ease: 'none'})
+// }
 
-barba.init({
-    transitions: [
-        {
-            once({next}){
-                animationEnter(next.container);
-            },
-            leave: ({current}) => animationLeave(current.container),
-            enter({next}){
-                animationEnter(next.container);
-            }
-        }
-    ]
-})
+// barba.init({
+//     transitions: [
+//         {
+//             once({next}){
+//                 animationEnter(next.container);
+//             },
+//             leave: ({current}) => animationLeave(current.container),
+//             enter({next}){
+//                 animationEnter(next.container);
+//             }
+//         }
+//     ]
+// })
