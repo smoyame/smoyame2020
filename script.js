c004edfda94c743333e949ada3a755b3dec9f3d5
smoyamedina <smoyamedina@outlook.com>
2020-10-04 22:06:39 -0400

    page anims and remove responsive jut

--- script.js
+++ script.js
@@ -20,6 +20,7 @@ let textReveal = (projNum, projTitle) => {
 }
 
 
+
 let projdescReveal = (projNum) => { 
     gsap.from (`.proj-desc${projNum}`, {
     duration: .8,
@@ -42,7 +43,7 @@ let projdescReveal = (projNum) => {
 
 textReveal(1, "the dry run")
 projdescReveal(1)
-textReveal(2, "amass magazine")
+textReveal(2, "amass mag")
 projdescReveal(2)
 textReveal(3, "voter's guide")
 projdescReveal(3)
@@ -67,56 +68,56 @@ gsap.to (".callmesam", {
 )
 
 
-// project pages // subslides
-const responsiveAnim = (subProjName, subDesc, subProjNameXVal) => {
+// subslides
+// const responsiveAnim = (subProjName, subDesc, subProjNameXVal) => {
 
-    let d = () => {
+//     let d = () => {
         
-        let proj1SubAnims = () => {
-            gsap.to (subProjName, {
-                duration: 1,
-                delay: .8,
-                x: subProjNameXVal,
-                ease: "power4.Out"
-                // x: 50,
-            }
-            )
+//         let proj1SubAnims = () => {
+//             gsap.to (subProjName, {
+//                 duration: 1,
+//                 delay: .8,
+//                 x: subProjNameXVal,
+//                 ease: "power4.Out"
+//                 // x: 50,
+//             }
+//             )
             
-            gsap.to (subDesc, {
-                duration: 1,
-                delay: 1.8,
-                opacity: 1,
-                ease: "power3.inOut"
-                // x: 50,
-            }
-            )
+//             gsap.to (subDesc, {
+//                 duration: 1,
+//                 delay: 1.8,
+//                 opacity: 1,
+//                 ease: "power3.inOut"
+//                 // x: 50,
+//             }
+//             )
             
-            }
-        if (innerWidth <= 1100) {
-            gsap.killTweensOf(subProjName);
-            gsap.set(subProjName, {
-                x: "0"
-            });
-            gsap.killTweensOf(subDesc);
+//             }
+//         if (innerWidth <= 1100) {
+//             gsap.killTweensOf(subProjName);
+//             gsap.set(subProjName, {
+//                 x: "0"
+//             });
+//             gsap.killTweensOf(subDesc);
             
-            gsap.set(subDesc, {
-                opacity: 1,
-            });
-        } else if (innerWidth > 1100) {
-            proj1SubAnims()
-        }
-    }
+//             gsap.set(subDesc, {
+//                 opacity: 1,
+//             });
+//         } else if (innerWidth > 1100) {
+//             proj1SubAnims()
+//         }
+//     }
     
-    addEventListener('resize', d)
-    addEventListener('DOMContentLoaded', d)
-    }
+//     addEventListener('resize', d)
+//     addEventListener('DOMContentLoaded', d)
+//     }
     
-    responsiveAnim(".sub-proj-name1", ".sub-desc1a", "-8vw")
-    responsiveAnim(".sub-proj-name2", ".sub-desc2a", "32vw")
-    responsiveAnim(".sub-proj-name3", ".sub-desc3a", "-8vw")
-    responsiveAnim(".sub-proj-name4", ".sub-desc4a", "-1.3vw")
-    responsiveAnim(".sub-proj-name5", ".sub-desc5a", "37vw")
-    responsiveAnim(".sub-proj-name6", ".sub-desc6a", "-7.8vw")
+//     responsiveAnim(".sub-proj-name1", ".sub-desc1a", "0")
+//     responsiveAnim(".sub-proj-name2", ".sub-desc2a", "0")
+//     responsiveAnim(".sub-proj-name3", ".sub-desc3a", "0")
+//     responsiveAnim(".sub-proj-name4", ".sub-desc4a", "0")
+//     responsiveAnim(".sub-proj-name5", ".sub-desc5a", "0")
+//     responsiveAnim(".sub-proj-name6", ".sub-desc6a", "0")
     
     
 
@@ -127,28 +128,26 @@ const responsiveAnim = (subProjName, subDesc, subProjNameXVal) => {
 
 
 const swup = new Swup();
+swup.on('animationOutDone', () => {
+    window.scrollTo(0, 0)
+})
 swup.on('contentReplaced', () => {
     //swup saves the scroll position on page transitions so i have to do this i'm so sorry
-    window.scrollTo(0, 0)
+    // window.scrollTo(0, 0)
 
     textReveal(1, "the dry run")
-projdescReveal(1)
-textReveal(2, "amass magazine")
-projdescReveal(2)
-textReveal(3, "voter's guide")
-projdescReveal(3)
-textReveal(4, "mailable poster")
-projdescReveal(4)
-textReveal(5, "chapbook")
-projdescReveal(5)
-textReveal(6, "misc materials")
-projdescReveal(6)
-responsiveAnim(".sub-proj-name1", ".sub-desc1a", "-8vw")
-    responsiveAnim(".sub-proj-name2", ".sub-desc2a", "32vw")
-    responsiveAnim(".sub-proj-name3", ".sub-desc3a", "-8vw")
-    responsiveAnim(".sub-proj-name4", ".sub-desc4a", "-1.3vw")
-    responsiveAnim(".sub-proj-name5", ".sub-desc5a", "37vw")
-    responsiveAnim(".sub-proj-name6", ".sub-desc6a", "-7.8vw")
+    projdescReveal(1)
+    textReveal(2, "amass magazine")
+    projdescReveal(2)
+    textReveal(3, "voter's guide")
+    projdescReveal(3)
+    textReveal(4, "mailable poster")
+    projdescReveal(4)
+    textReveal(5, "chapbook")
+    projdescReveal(5)
+    textReveal(6, "misc materials")
+    projdescReveal(6)
+    
     gsap.to (".callmesam", {
         duration: 1,
         delay: .2,
