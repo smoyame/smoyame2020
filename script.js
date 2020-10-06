ddf2813de0993ccc6c6a0349f9b3c95bf2c19724
smoyamedina <smoyamedina@outlook.com>
2020-10-05 20:33:18 -0400

    set up new sidebar

---
+++ script.js
@@ -0,0 +1,57 @@
+let textReveal = (projNum, projTitle) => {
+    gsap.to (`.proj-name${projNum}`, {
+        duration: .5,
+        fontWeight: "600",
+        text: projTitle,
+        ease: "linear",
+        
+        // x: 50,
+        
+        scrollTrigger: {
+            trigger:`.slide${projNum}`,
+            // markers: true,
+            start: "top 75%",
+            end: "bottom: 0%",
+            // end: "50%",
+            toggleActions: "play reverse play reverse",
+            
+        }
+    
+    }
+    )
+}
+
+
+
+let projdescReveal = (projNum) => { 
+    gsap.from (`.proj-desc${projNum}`, {
+    duration: .8,
+    opacity: "0",
+    // ease: "linear",
+    // clipPath: "inset(0% 0% 0% 100%)",
+    
+    scrollTrigger: {
+        trigger: `.slide${projNum}`,
+        start: "top 75%",
+        end: "bottom: 0%",
+        // markers: true,
+        toggleActions: "play reverse play reverse",
+    }
+}
+)
+}
+
+
+
+textReveal(1, "the dry run »")
+projdescReveal(1)
+textReveal(2, "amass mag »")
+projdescReveal(2)
+textReveal(3, "voter's guide »")
+projdescReveal(3)
+textReveal(4, "mailable poster »")
+projdescReveal(4)
+textReveal(5, "chapbook »")
+projdescReveal(5)
+textReveal(6, "misc materials »")
+projdescReveal(6)
