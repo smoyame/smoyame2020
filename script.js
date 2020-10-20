cb0b7342fb229c32f9b28b6137603ff9a34bca4d
smoyamedina <smoyamedina@outlook.com>
2020-10-20 15:31:19 -0400

    all new files for new version

---
+++ script.js
@@ -0,0 +1,109 @@
+
+//hero text up in
+let tl = gsap.timeline()
+
+tl.from('.hidetext', {
+    opacity: 0,
+    delay: .5,
+    duration: 1,
+    stagger: .3,
+    y: "125%",
+    ease: "power4.inOut"
+}
+)
+
+
+//project slides
+
+let oddProj = (projNum) => {
+gsap.from (`.projdesc${projNum}`, {
+    opacity: 0,
+    x: "10%",
+    ease: "power3.inOut",
+    scrollTrigger: {
+        trigger: `.project${projNum}`,
+        start: "top 50%",
+            end: "bottom 50%",
+        // markers: true,
+        toggleActions: "play reverse play reverse",
+    }
+}
+)
+}
+
+let evenProj = (projNum) => {
+    gsap.from (`.projdesc${projNum}`, {
+        opacity: 0,
+        x: "-10%",
+        ease: "power3.inOut",
+        scrollTrigger: {
+            trigger: `.project${projNum}`,
+            // start: "top 45%",
+            // end: "-10%",
+
+            start: "top 50%",
+            end: "bottom 50%",
+            // markers: true,
+            toggleActions: "play reverse play reverse",
+        }
+    }
+    )
+    }
+
+oddProj(1);
+evenProj(2);
+oddProj(3);
+evenProj(4);
+oddProj(5);
+evenProj(6);
+
+
+
+
+
+
+//SWUP 
+const swup = new Swup();
+
+swup.on('animationOutDone', () => {
+    window.scrollTo(0, 0)   
+})
+
+//SUB PAGES
+
+gsap.from('.sub-proj-desc', {
+    opacity: 0,
+    duration: 1,
+    ease: "power4.inOut",
+    x:"-25%"
+})
+
+
+swup.on('contentReplaced', () => {
+
+    oddProj(1);
+    evenProj(2);
+    oddProj(3);
+    evenProj(4);
+    oddProj(5);
+    evenProj(6);
+
+    gsap.from('.sub-proj-desc', {
+        opacity: 0,
+        duration: 1,
+        ease: "power4.inOut",
+        x:"-25%"
+    })
+
+
+    $(function () {
+        $('.sub-images a').fluidbox();
+    })
+}
+)
+
+/// fluidbox
+
+$(function () {
+    $('.sub-images a').fluidbox();
+})
