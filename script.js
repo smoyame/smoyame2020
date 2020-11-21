4266561f10e75bf447c8611f6ae8fd16b3f2e0c1
smoyamedina <smoyamedina@outlook.com>
2020-11-20 19:48:32 -0500

    new website

--- script.js
+++ script.js
@@ -1,419 +1,143 @@
-//hero
-
-let tl = gsap.timeline()
-
-tl.from('.hidetext', {
+//tagline animation
+gsap.from('.tagline', {
+    duration: .5,
     opacity: 0,
-    delay: .15,
-    duration: 1,
-    stagger: .1,
-    y: "125%",
-    ease: "power4.out"
-})
-
-
-
-///////////
-
-//proj info animations
-
-let oddProj = (projNum) => {
-    gsap.from(`.proj-info${projNum}`, {
-        opacity: 0,
-        x: "10%",
-        ease: "power3.inOut",
-        scrollTrigger: {
-            trigger: `.project${projNum}`,
-            start: "top 50%",
-            end: "bottom 50%",
-            // markers: true,
-            toggleActions: "play none play none",
+    // text:" ",
+    // padSpace: true,
+    ease: "power4.inOut",
+    scrollTrigger: {
+        trigger:'.hero',
+        // markers: true,
+        start: "top 50%",
+        end: "bottom 60%",
+        toggleActions: "play reverse play reverse",
+        
         }
-    })
+    }
+)
+
+
+//project slides on home page
+let textReveal = (projNum) => {
+    gsap.from(`.proj-name${projNum}`, {
+        duration: .5,
+        opacity: 0,
+        text:" ",
+        ease: "power4.inOut",
+        scrollTrigger: {
+            trigger:`.project${projNum}`,
+            // markers: true,
+            start: "top 50%",
+            end: "bottom 60%",
+            toggleActions: "play reverse play reverse",
+            
+            }
+        }
+    )
 }
 
-let evenProj = (projNum) => {
-    gsap.from(`.proj-info${projNum}`, {
-        opacity: 0,
-        x: "-10%",
-        ease: "power3.inOut",
-        scrollTrigger: {
-            trigger: `.project${projNum}`,
-            // start: "top 45%",
-            // end: "-10%",
-
-            start: "top 50%",
-            end: "bottom 50%",
-            // markers: true,
-            toggleActions: "play none play none",
-        }
-    })
-}
-
-oddProj(1);
-evenProj(2);
-oddProj(3);
-evenProj(4);
-oddProj(5);
-evenProj(6);
-oddProj(7);
-
-
-
-// parallax animation
-
-gsap.from('.project1', {
-    y: 300,
-    scrollTrigger: {
-        trigger: '.projects',
-        scrub: 1
-    }
-})
-
-gsap.from('.project2', {
-    y: 150,
-    scrollTrigger: {
-        trigger: 'body',
-        scrub: 1
-    }
-})
-
-gsap.from('.project3', {
-    y: 200,
-    scrollTrigger: {
-        trigger: 'body',
-        scrub: 1
-    }
-})
-
-
-
-gsap.from('.project4', {
-    y: 300,
-    scrollTrigger: {
-        trigger: '.projects',
-        scrub: 1
-    }
-})
-
-gsap.from('.project5', {
-    y: 200,
-    scrollTrigger: {
-        trigger: 'body',
-        scrub: 1
-    }
-})
-
-gsap.from('.project6', {
-    y: 250,
-    scrollTrigger: {
-        trigger: 'body',
-        scrub: 1
-    }
-})
-
-gsap.from('.project7', {
-    y: 300,
-    scrollTrigger: {
-        trigger: 'body',
-        scrub: 1
-    }
-})
-
-
-/////////////////////////////////////// SUB PAGES //
-// SUB PAGES ///////////////////////////////////////
-/////////////////////////////////////// SUB PAGES //
-
-gsap.from('.sub-hero-img', {
-    y: 165,
-    scrollTrigger: {
-        trigger: 'body',
-        scrub: 1
-    }
-})
-
-gsap.from('.sub-text-contain', {
-    y: 80,
-    scrollTrigger: {
-        trigger: 'body',
-        scrub: 1
-    }
-})
-
-gsap.from('.sub-bgblock1a', {
-    y: 100,
-    scrollTrigger: {
-        trigger: 'body',
-        scrub: 1
-    }
-})
-
-gsap.from('.sub-bgblock1b', {
-    y: 250,
-    scrollTrigger: {
-        trigger: 'body',
-        scrub: 1
-    }
-})
-
-gsap.from('.sub-bgblock1c', {
-    y: 190,
-    scrollTrigger: {
-        trigger: 'body',
-        scrub: 1
-    }
-})
-
-gsap.from('.sub-bgblock2a', {
-    y: 100,
-    scrollTrigger: {
-        trigger: 'body',
-        scrub: 1
-    }
-})
-
-gsap.from('.sub-bgblock2b', {
-    y: 250,
-    scrollTrigger: {
-        trigger: 'body',
-        scrub: 1
-    }
-})
-
-gsap.from('.sub-bgblock2c', {
-    y: 190,
-    scrollTrigger: {
-        trigger: 'body',
-        scrub: 1
-    }
-})
-
-gsap.from('.sub-bgblock3a', {
-    y: 100,
-    scrollTrigger: {
-        trigger: 'body',
-        scrub: 1
-    }
-})
-
-gsap.from('.sub-bgblock3b', {
-    y: 250,
-    scrollTrigger: {
-        trigger: 'body',
-        scrub: 1
-    }
-})
-
-gsap.from('.sub-bgblock3c', {
-    y: 190,
-    scrollTrigger: {
-        trigger: 'body',
-        scrub: 1
-    }
-})
+textReveal(1)
+textReveal(2)
+textReveal(3)
+textReveal(4)
+textReveal(5)
+textReveal(6)
+textReveal(7)
 
 //medium lightbox
 MediumLightbox('figure.zoom-effect');
 
+
 //swup
-// const swup = new Swup(); 
 
+// const swup = new Swup();
 
-swup.on('contentReplaced', () => {
-    //hero
-    let tl = gsap.timeline()
+// swup.on('contentLoaded', () => {
+//     MediumLightbox('figure.zoom-effect');
+//     textReveal(1)
+//     textReveal(2)
+//     textReveal(3)
+//     textReveal(4)
+//     textReveal(5)
+//     textReveal(6)
+//     textReveal(7)
 
-    tl.from('.hidetext', {
-        opacity: 0,
-        delay: .15,
-        duration: 1,
-        stagger: .1,
-        y: "125%",
-        ease: "power4.out"
-    })
-
-
-    //front page
-
-    oddProj(1);
-    evenProj(2);
-    oddProj(3);
-    evenProj(4);
-    oddProj(5);
-    evenProj(6);
-    oddProj(7);
-
-    // parallax animation
-
-    gsap.from('.project1', {
-        y: 300,
-        scrollTrigger: {
-            trigger: '.projects',
-            scrub: 1
-        }
-    })
-
-    gsap.from('.project2', {
-        y: 150,
-        scrollTrigger: {
-            trigger: 'body',
-            scrub: 1
-        }
-    })
-
-    gsap.from('.project3', {
-        y: 200,
-        scrollTrigger: {
-            trigger: 'body',
-            scrub: 1
-        }
-    })
+//     gsap.from('.tagline', {
+//         duration: .5,
+//         opacity: 0,
+//         // text:" ",
+//         // padSpace: true,
+//         ease: "power4.inOut",
+//         scrollTrigger: {
+//             trigger:'.hero',
+//             // markers: true,
+//             start: "top 50%",
+//             end: "bottom 60%",
+//             toggleActions: "play reverse play reverse",
+            
+//             }
+//         }
+//     )
+// }
+// )
 
 
 
-    gsap.from('.project4', {
-        y: 300,
-        scrollTrigger: {
-            trigger: '.projects',
-            scrub: 1
-        }
-    })
-
-    gsap.from('.project5', {
-        y: 200,
-        scrollTrigger: {
-            trigger: 'body',
-            scrub: 1
-        }
-    })
-
-    gsap.from('.project6', {
-        y: 250,
-        scrollTrigger: {
-            trigger: 'body',
-            scrub: 1
-        }
-    })
-
-    gsap.from('.project7', {
-        y: 300,
-        scrollTrigger: {
-            trigger: 'body',
-            scrub: 1
-        }
-    })
 
 
-    /////////////////////////////////////// SUB PAGES //
-    // SUB PAGES ///////////////////////////////////////
-    /////////////////////////////////////// SUB PAGES //
-
-    gsap.from('.sub-hero-img', {
-        y: 165,
-        scrollTrigger: {
-            trigger: 'body',
-            scrub: 1
-        }
-    })
-
-    gsap.from('.sub-text-contain', {
-        y: 80,
-        scrollTrigger: {
-            trigger: 'body',
-            scrub: 1
-        }
-    })
-
-    gsap.from('.sub-bgblock1a', {
-        y: 100,
-        scrollTrigger: {
-            trigger: 'body',
-            scrub: 1
-        }
-    })
-
-    gsap.from('.sub-bgblock1b', {
-        y: 250,
-        scrollTrigger: {
-            trigger: 'body',
-            scrub: 1
-        }
-    })
-
-    gsap.from('.sub-bgblock1c', {
-        y: 190,
-        scrollTrigger: {
-            trigger: 'body',
-            scrub: 1
-        }
-    })
-
-    gsap.from('.sub-bgblock2a', {
-        y: 100,
-        scrollTrigger: {
-            trigger: 'body',
-            scrub: 1
-        }
-    })
-
-    gsap.from('.sub-bgblock2b', {
-        y: 250,
-        scrollTrigger: {
-            trigger: 'body',
-            scrub: 1
-        }
-    })
-
-    gsap.from('.sub-bgblock2c', {
-        y: 190,
-        scrollTrigger: {
-            trigger: 'body',
-            scrub: 1
-        }
-    })
-
-    gsap.from('.sub-bgblock3a', {
-        y: 100,
-        scrollTrigger: {
-            trigger: 'body',
-            scrub: 1
-        }
-    })
-
-    gsap.from('.sub-bgblock3b', {
-        y: 250,
-        scrollTrigger: {
-            trigger: 'body',
-            scrub: 1
-        }
-    })
-
-    gsap.from('.sub-bgblock3c', {
-        y: 190,
-        scrollTrigger: {
-            trigger: 'body',
-            scrub: 1
-        }
-    })
-
-    //medium lightbox
-    MediumLightbox('figure.zoom-effect');
 
 
-});
 
-// swup scroll cheat
-swup.on('animationOutDone', () => {
-    window.scrollTo(0, 0)   
-})
 
-//reload on back
-jQuery( document ).ready(function( $ ) {
+// // cursor movement
+// // thanks to someone on codepen
+// // $(document).on("click mousemove",".container",function(e){ 
+// //     var x = e.clientX;
+// //     var y = e.clientY;
+// //     var newposX = x - 60;
+// //     var newposY = y - 60; $("#cursor").css("transform","translate3d("+newposX+"px,"+newposY+"px,0px)");
+// //     });
 
-    //Use this inside your document ready jQuery 
-    $(window).on('popstate', function() {
-       location.reload(true);
-    });
- 
- });
+// // //cursor hover effect
+
+// // let cursor = document.getElementById("cursor");
+
+// // let cursorColorChange = (color) => {
+// //     cursor.style.backgroundColor = color;
+// // }
+
+// //////SWUP
+
+// // const swup = new Swup();
+// // swup.on('animationOutDone', () => {
+// //     window.scrollTo(0, 0)
+// //     // projdescReveal(1)
+// //     // projdescReveal(2)
+   
+// // })
+// // swup.on('contentReplaced', () => {
+// //     //swup saves the scroll position on page transitions so i have to do this i'm so sorry
+// //     // window.scrollTo(0, 0)
+
+// //     textReveal1(1, "the dry run »")
+// //     projdescReveal1(1)
+// //     textReveal(2, "olympics »")
+// //     projdescReveal(2)
+// //     textReveal(3, "amass mag »")
+// //     projdescReveal(3)
+// //     textReveal(4, "your mail »")
+// //     projdescReveal(4)
+// //     textReveal(5, "voter's guide »")
+// //     projdescReveal(5)
+// //     textReveal(6, "misc materials »")
+// //     projdescReveal(6)
+    
+// //     gsap.to (".callmesam", {
+// //         duration: 1,
+// //         delay: .2,
+// //         text: "You can call me Sam!",
+// //         ease: "linear",
+// //         // x: 50,
+// //     }
+// //     )
+// // })
