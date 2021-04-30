fc9dd8ab25f728cbb2bd342a497f0f1c1b278b0e
smoyamedina <smoyamedina@outlook.com>
2021-04-30 14:06:57 -0400

    stacking error with _script.pug

--- script.js
+++ script.js
@@ -126,56 +126,7 @@ textReveal(8)
 //medium lightbox
 MediumLightbox('figure.zoom-effect');
 
-//cursor courtesy of Nikita on codepen
-//https://codepen.io/chloyka/pen/VwZeRdy
 
-var cursor, links;
-cursor = document.querySelector('.custom-cursor');
-links = document.querySelectorAll('a');
-btn = document.querySelector('button');
-window.initCursor = false;
-for (var i = 0; i < links.length; i++) {
-  var selfLink = links[i];
-
-  selfLink.addEventListener("mouseover", function() {
-    cursor.classList.add("custom-cursor--link");
-  });
-  selfLink.addEventListener("mouseout", function() {
-    cursor.classList.remove("custom-cursor--link");
-  });
-}
-
-window.onmousemove = function (e) {
-  var mouseX, mouseY;
-  mouseX = e.clientX;
-  mouseY = e.clientY;
-  if (!window.initCursor) {
-
-    jQuery(document).find('.custom-cursor').css({
-      opacity: 1
-    });
-    window.initCursor = true;
-  }
-  jQuery(document).find('.custom-cursor').css({
-    top: mouseY + 'px',
-    left: mouseX + 'px'
-  })
-};
-window.onmouseout = function (e) {
-  jQuery(document).find('.custom-cursor').css({
-    opacity: 0
-  });
-  window.initCursor = false;
-};
-
-if (btn !== null) {
-  btn.addEventListener("mouseover", function() {
-    cursor.classList.add("custom-cursor--link");
-  });
-  btn.addEventListener("mouseout", function() {
-    cursor.classList.remove("custom-cursor--link");
-  });
-}
 
 
 //accordion testing - scrapped first run with animation. has extra click at start to set then it works, but then theres also making it work across mobile. gah!!!!
@@ -259,185 +210,53 @@ Promise.all(Array.from(document.images).filter(img => !img.complete).map(img => 
 
 // from https://stackoverflow.com/questions/11071314/javascript-execute-after-all-images-have-loaded
 
-//document.getElementsByClassName("aspect-ratio-fill").closest(".process-panel")
+//cursor courtesy of Nikita on codepen
+//https://codepen.io/chloyka/pen/VwZeRdy
 
-// applySize(0)
-// applySize(1)
+var cursor, links;
+cursor = document.querySelector('.custom-cursor');
+links = document.querySelectorAll('a');
+btn = document.querySelector('button');
+window.initCursor = false;
+for (var i = 0; i < links.length; i++) {
+  var selfLink = links[i];
 
-// for (i = 0; i < acc.length; i++) {
-//   acc[i].addEventListener("click", function() {
-//     let process = document.querySelector(".process-contain");
-//     let cacheProcessHeight = getComputedStyle(process).height;
+  selfLink.addEventListener("mouseover", function() {
+    cursor.classList.add("custom-cursor--link");
+  });
+  selfLink.addEventListener("mouseout", function() {
+    cursor.classList.remove("custom-cursor--link");
+  });
+}
 
+window.onmousemove = function (e) {
+  var mouseX, mouseY;
+  mouseX = e.clientX;
+  mouseY = e.clientY;
+  if (!window.initCursor) {
 
-//     this.classList.toggle("active");
-//     var panel = document.querySelector(".work-process");
-//     if (panel.style.maxHeight) {
-//       panel.style.height = "4vw";
-//       panel.style.overflow = "hidden"
-//     } else {
-//     //   panel.style.maxHeight = panel.scrollHeight + "px";
-//     panel.style.height = "auto"
-//     panel.style.overflow = "visible"
-//   }
+    jQuery(document).find('.custom-cursor').css({
+      opacity: 1
+    });
+    window.initCursor = true;
+  }
+  jQuery(document).find('.custom-cursor').css({
+    top: mouseY + 'px',
+    left: mouseX + 'px'
+  })
+};
+window.onmouseout = function (e) {
+  jQuery(document).find('.custom-cursor').css({
+    opacity: 0
+  });
+  window.initCursor = false;
+};
 
-
-//   });
-
-//   collapseBtn[i].addEventListener("click", function() {
-//     let process = document.querySelector(".process-contain");
-//     let cacheProcessHeight = getComputedStyle(process).height;
-
-
-//     this.classList.toggle("active");
-//     var panel = document.querySelector(".process-contain");
-//     if (panel.style.maxHeight) {
-//       // panel.style.maxHeight = null;
-//       panel.style.overflow = "hidden"
-
-//     } else {
-//     //   panel.style.maxHeight = panel.scrollHeight + "px";
-//       panel.style.maxHeight = "inherit"
-//       panel.style.overflow = "visible"
-//   }
-
-//   var idP = document.getElementById("process");
-//       idP.scrollIntoView();
-
-
-//   });
-// }
-
-// FIRST RUN
-// var acc = document.getElementsByClassName("accordion");
-// var i;
-
-
-// let initContainer = document.querySelector(".work-process");
-// let cacheContainerHeight = getComputedStyle(initContainer).height;
-
-// for (i = 0; i < acc.length; i++) {
-//   acc[i].addEventListener("click", function() {
-//     /* Toggle between adding and removing the "active" class,
-//     to highlight the button that controls the panel */
-    
-
-//     this.classList.toggle("active");
-
-//     let process = document.querySelector(".process-contain");
-//     let cacheProcessHeight = getComputedStyle(process).height;
-//     let container = document.querySelector(".work-process");
-    
-
-    
-    
-//     /* Toggle between hiding and showing the active panel */
-    
-//     if (container.style.maxHeight !== "4vw") {
-//         container.style.maxHeight = "4vw";
-//     } else {
-//         container.style.maxHeight = cacheProcessHeight;
-//     }
-
-//   });
-// }
-
-
-
-// mediaQuery.addListener(handleSizeChange)
-
-// handleSizeChange(mediaQuery);
-
-//swup
-
-// const swup = new Swup();
-
-// swup.on('contentLoaded', () => {
-//     MediumLightbox('figure.zoom-effect');
-//     textReveal(1)
-//     textReveal(2)
-//     textReveal(3)
-//     textReveal(4)
-//     textReveal(5)
-//     textReveal(6)
-//     textReveal(7)
-
-//     gsap.from('.tagline', {
-//         duration: .5,
-//         opacity: 0,
-//         // text:" ",
-//         // padSpace: true,
-//         ease: "power4.inOut",
-//         scrollTrigger: {
-//             trigger:'.hero',
-//             // markers: true,
-//             start: "top 50%",
-//             end: "bottom 60%",
-//             toggleActions: "play reverse play reverse",
-            
-//             }
-//         }
-//     )
-// }
-// )
-
-
-
-
-
-
-
-
-
-// // cursor movement
-// // thanks to someone on codepen
-// // $(document).on("click mousemove",".container",function(e){ 
-// //     var x = e.clientX;
-// //     var y = e.clientY;
-// //     var newposX = x - 60;
-// //     var newposY = y - 60; $("#cursor").css("transform","translate3d("+newposX+"px,"+newposY+"px,0px)");
-// //     });
-
-// // //cursor hover effect
-
-// // let cursor = document.getElementById("cursor");
-
-// // let cursorColorChange = (color) => {
-// //     cursor.style.backgroundColor = color;
-// // }
-
-// //////SWUP
-
-// // const swup = new Swup();
-// // swup.on('animationOutDone', () => {
-// //     window.scrollTo(0, 0)
-// //     // projdescReveal(1)
-// //     // projdescReveal(2)
-   
-// // })
-// // swup.on('contentReplaced', () => {
-// //     //swup saves the scroll position on page transitions so i have to do this i'm so sorry
-// //     // window.scrollTo(0, 0)
-
-// //     textReveal1(1, "the dry run »")
-// //     projdescReveal1(1)
-// //     textReveal(2, "olympics »")
-// //     projdescReveal(2)
-// //     textReveal(3, "amass mag »")
-// //     projdescReveal(3)
-// //     textReveal(4, "your mail »")
-// //     projdescReveal(4)
-// //     textReveal(5, "voter's guide »")
-// //     projdescReveal(5)
-// //     textReveal(6, "misc materials »")
-// //     projdescReveal(6)
-    
-// //     gsap.to (".callmesam", {
-// //         duration: 1,
-// //         delay: .2,
-// //         text: "You can call me Sam!",
-// //         ease: "linear",
-// //         // x: 50,
-// //     }
-// //     )
-// // })
+if (btn !== null) {
+  btn.addEventListener("mouseover", function() {
+    cursor.classList.add("custom-cursor--link");
+  });
+  btn.addEventListener("mouseout", function() {
+    cursor.classList.remove("custom-cursor--link");
+  });
+}
